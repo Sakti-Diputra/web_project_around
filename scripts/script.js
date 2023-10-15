176b9e395eb23c1ca80106fd76942e3eeb993aec
Sakti Diputra Akbar <saktidiputra14@gmail.com>
2023-10-15 15:29:51 +0800

    modified

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,161 @@
-// Ambil referensi elemen
-const initialCards = [
-    {
-      name: "Lembah Yosemite",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
-    },
-    {
-      name: "Danau Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
-    },
-    {
-      name: "Pegunungan Gundul",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
-    },
-    {
-      name: "Gunung Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
-    },
-    {
-      name: "Taman Nasional Vanoise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
-    }
-  ];
-
+// Section 1: Mendeklarasikan Variabel
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
-const popupEdit = document.querySelector('.popup_edit');
-const popupAdd = document.querySelector('.popup_add');
-const nameProfileElement = document.querySelector('.profile__name');
-const aboutMeProfileElement = document.querySelector('.profile__about');
-const inputName = popupEdit.querySelector('.popup__input_name');
-const inputAboutMe = popupEdit.querySelector('.popup__input_about-me');
 const closeButton = document.querySelector('.popup__close-button');
-const editProfileForm = document.querySelector('.popup__container[name="edit-profile-form"]');
-const newPlaceForm = document.querySelector('.popup__container[name="new-place-form"]');
-const titleInput = popupAdd.querySelector('.popup__input_title');
-const imageUrlInput = popupAdd.querySelector('.popup__input_image-url');
+const closeImageButton = document.querySelector('.popup__close-image');
 
-// Fungsi untuk mengisi nilai input dari tampilan profil
-function isiInputDariProfil() {
-    inputName.value = nameProfileElement.textContent;
-    inputAboutMe.value = aboutMeProfileElement.textContent;
+const popupEdit = document.querySelector('.popup__edit');
+const popupAdd = document.querySelector('.popup__add');
+const popupImages = document.querySelector('.popup__images');
+
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+
+const inputName = document.querySelector('.popup__input_name');
+const inputAbout = document.querySelector('.popup__input_about-me');
+
+const inputTitle = document.querySelector('.popup__input_title');
+const inputImage = document.querySelector('.popup__input_image-url');
+
+const saveEditProfile = document.querySelector('form[name="edit-profile-form"]');
+const saveAdd = document.querySelector('form[name="new-place-form"]');
+
+const elements = document.querySelector('.elements');
+const cardTemplate = document.querySelector('.card__template');
+
+const popupImage = document.querySelector('.popup__image');
+const popupTitleImage = document.querySelector('.popup__title-image');
+
+// Source of data
+// Aray of object
+const boxCards = [
+  {
+    name: "Lembah Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Danau Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Pegunungan Gundul",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Gunung Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Taman Nasional Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+
+
+// Section 2: Pembuatan Fungsi
+// ---------------------------
+
+// Function render template
+// Untuk menampilkan carad card dari template
+function renderTemplate(data) {
+
+  //clean elements
+  elements.innerHTML = '';
+  console.log(data);
+
+  //inisialisasi; jumlah data; increment
+  for (let i = 0; i < data.length; i++){ 
+
+    const placeImage = data[i].link;
+    const placeName = data[i].name;
+    //Template
+    const cardTemplateContent = cardTemplate.content
+    const cloneContent = cardTemplateContent.cloneNode(true)
+
+    //Mengubah nilai Image Url dan Title
+    const cardImage = cloneContent.querySelector('.card__image');
+    const cardTitle = cloneContent.querySelector('.card__title');
+  
+    cardImage.src = placeImage;
+    cardTitle.textContent = placeName;
+
+    // -Eventlistener
+    cardImage.addEventListener('click', function() {
+      console.log(placeName)
+      // Toggle popup image
+      popupImages.classList.toggle('popup_opened');
+
+      // Inject data
+      popupImage.src = placeImage;
+      popupTitleImage.textContent = placeName;
+
+    });
+
+    const deleteCard = cloneContent.querySelector('.card__delete-button');
+    
+    deleteCard.addEventListener('click', function() {
+      console.log('delete', placeName)
+
+      // Delete
+      boxCards.splice(i, 1);
+      renderTemplate(boxCards)
+    });
+
+    const likeButton = cloneContent.querySelector('.card__like-button');
+    likeButton.addEventListener('click', function() {
+      console.log(`like: ${placeName}`)
+
+      // menambahkan fa-solid
+      const child = likeButton.children;
+      console.log(typeof(child), child)
+
+      const heart = child[0];
+      console.log(heart)
+      heart.classList.add('fa-solid')
+    });
+
+    // -Append
+    elements.appendChild(cloneContent);
+  
+  }
+};
+
+// Fungsi edit profile
+// Untuk mengubah informasi profile
+function editProfile() {
+  console.log('edit button')
+
+  // Toggle popup edit
+  popupEdit.classList.toggle('popup_opened')
+
+  // Inject data ke input name
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
 }
 
-// Fungsi untuk mengupdate tampilan profil dari nilai input
-function perbaruiProfilDariInput() {
-    nameProfileElement.textContent = inputName.value;
-    aboutMeProfileElement.textContent = inputAboutMe.value;
-}
+// Section 3: Mendaftarkan Event Listener
+// ---------------------------------------
 
-// Menambahkan event listener untuk tombol "Edit Profil"
-editButton.addEventListener('click', () => {
-    popupEdit.classList.add('popup_opened'); // Tampilkan popupEdit
-    isiInputDariProfil(); // Isi nilai input dengan nilai dari profil saat tombol "Edit Profil" ditekan
+editButton.addEventListener('click', editProfile);
+
+saveEditProfile.addEventListener('submit', function (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+
+  popupEdit.classList.toggle('popup_opened');
+
 });
 
-// Menambahkan event listener untuk tombol "Add Card"
-addButton.addEventListener('click', () => {
-    popupAdd.classList.add('popup_opened');
-    isiInputDariCard();
+addButton.addEventListener('click', function() {
+  console.log('add button')
+
+  popupAdd.classList.toggle('popup_opened')
 });
 
 // Menggunakan event delegation untuk menangani tombol "Tutup" pada semua popup
@@ -76,47 +170,31 @@ document.addEventListener('click', (event) => {
   }
 });
 
+saveAdd.addEventListener('submit', function (event) {
+  const title = inputTitle.value;
+  const image = inputImage.value;
 
-// Menambahkan event listener untuk event submit pada formulir
-editProfileForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // Hindari aksi bawaan formulir
-    perbaruiProfilDariInput(); // Perbarui tampilan profil dari nilai input
-    popupEdit.classList.remove('popup_opened'); // Sembunyikan popup setelah submit
+  // Validation
+
+  console.log(`
+    Title: ${title},
+    Image: ${image}
+  `);
+
+  boxCards.push({
+    name: title,
+    link: image
+  });
+
+  renderTemplate(boxCards);
+
+  popupAdd.classList.toggle('popup_opened');
 });
 
-// Menambahkan event listener untuk event submit pada formulir popup tambahan (popup add)
-newPlaceForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // Hindari aksi bawaan formulir
-
-  // Ambil nilai yang dimasukkan pengguna
-  const title = titleInput.value;
-  const imageUrl = imageUrlInput.value;
-
-  // Panggil fungsi untuk menambahkan kartu baru
-  handleAddCard(title, imageUrl);
-
-  // Reset nilai input
-  titleInput.value = '';
-  imageUrlInput.value = '';
-
-  popupAdd.classList.remove('popup_opened'); // Sembunyikan popup setelah submit
+closeImageButton.addEventListener('click', function () {
+  popupImages.classList.toggle('popup_opened')
 });
 
-
-
-const templateCard = document.querySelector('.template__card').content;
-const elements = document.querySelector('.elements');
-initialCards.forEach(i =>{
-    const templateCardCopy = templateCard.cloneNode(true);
-    templateCardCopy.querySelector(".card__title").textContent = i.name;
-    templateCardCopy.querySelector(".card__image").src = i.link;
-    elements.append(templateCardCopy);
-});
-
-function handleAddCard(name,link) {
-    const templateCardCopy = templateCard.cloneNode(true);
-    templateCardCopy.querySelector(".card__title").textContent = name;
-    templateCardCopy.querySelector(".card__image").src = link;
-    elements.append(templateCardCopy);
-}
-
+// --------
+// Inisialisasi
+renderTemplate(boxCards);
