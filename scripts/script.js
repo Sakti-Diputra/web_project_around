49b9e24e3fe2717c4a560042f39da6c3b9dc212d
Sakti Diputra Akbar <saktidiputra14@gmail.com>
2023-10-17 20:15:34 +0800

    modified

--- scripts/script.js
+++ scripts/script.js
@@ -27,7 +27,6 @@ const popupImage = document.querySelector('.popup__image');
 const popupTitleImage = document.querySelector('.popup__title-image');
 
 // Source of data
-// Aray of object
 const boxCards = [
   {
     name: "Lembah Yosemite",
@@ -55,102 +54,59 @@ const boxCards = [
   }
 ];
 
-
-
 // Section 2: Pembuatan Fungsi
-// ---------------------------
+function createCard(data) {
+  const placeImage = data.link;
+  const placeName = data.name;
+  const cardTemplateContent = cardTemplate.content.cloneNode(true);
 
-// Function render template
-// Untuk menampilkan carad card dari template
+  const cardImage = cardTemplateContent.querySelector('.card__image');
+  const cardTitle = cardTemplateContent.querySelector('.card__title');
+  const deleteCard = cardTemplateContent.querySelector('.card__delete-button');
+  const likeButton = cardTemplateContent.querySelector('.card__like-button');
+
+  cardImage.src = placeImage;
+  cardTitle.textContent = placeName;
+
+  cardImage.addEventListener('click', function() {
+    popupImages.classList.toggle('popup_opened');
+    popupImage.src = placeImage;
+    popupTitleImage.textContent = placeName;
+  });
+
+  deleteCard.addEventListener('click', function() {
+    const cardToDelete = deleteCard.closest('.card');
+    elements.removeChild(cardToDelete);
+  });
+
+  likeButton.addEventListener('click', function() {
+    likeButton.classList.toggle('liked');
+    if (likeButton.classList.contains('liked')) {
+      likeButton.src = './images/button_like-on.svg';
+    } else {
+      likeButton.src = './images/Like_button.svg';
+    }
+  });
+
+  return cardTemplateContent;
+}
+
 function renderTemplate(data) {
-
-  //clean elements
   elements.innerHTML = '';
-  console.log(data);
 
-  //inisialisasi; jumlah data; increment
-  for (let i = 0; i < data.length; i++){ 
+  data.forEach(function(cardData) {
+    const card = createCard(cardData);
+    elements.appendChild(card);
+  });
+}
 
-    const placeImage = data[i].link;
-    const placeName = data[i].name;
-    //Template
-    const cardTemplateContent = cardTemplate.content
-    const cloneContent = cardTemplateContent.cloneNode(true)
-
-    //Mengubah nilai Image Url dan Title
-    const cardImage = cloneContent.querySelector('.card__image');
-    const cardTitle = cloneContent.querySelector('.card__title');
-  
-    cardImage.src = placeImage;
-    cardTitle.textContent = placeName;
-
-    // -Eventlistener
-    cardImage.addEventListener('click', function() {
-      console.log(placeName)
-      // Toggle popup image
-      popupImages.classList.toggle('popup_opened');
-
-      // Inject data
-      popupImage.src = placeImage;
-      popupTitleImage.textContent = placeName;
-
-    });
-
-    const deleteCard = cloneContent.querySelector('.card__delete-button');
-    
-    deleteCard.addEventListener('click', function() {
-      console.log('delete', placeName)
-
-      // Delete
-      boxCards.splice(i, 1);
-      renderTemplate(boxCards)
-    });
-
-    const likeButton = cloneContent.querySelector('.card__like-button');
-    likeButton.addEventListener('click', function() {
-      console.log(likeButton)
-      const statusBtn = likeButton.src.indexOf('Like_button.svg')
-      console.log(statusBtn)
-      
-      if (statusBtn > -1) { 
-        likeButton.src = './images/button_like-on.svg'
-      }
-
-      else {
-        likeButton.src = './images/Like_button.svg'
-      }
-
-      // menambahkan fa-solid
-      //const child = likeButton.children;
-     // console.log(typeof(child), child)
-
-     // const heart = child[0];
-     // console.log(heart)
-     // heart.classList.add('fa-solid')
-    });
-
-    // -Append
-    elements.appendChild(cloneContent);
-  
-  }
-};
-
-// Fungsi edit profile
-// Untuk mengubah informasi profile
 function editProfile() {
-  console.log('edit button')
-
-  // Toggle popup edit
-  popupEdit.classList.toggle('popup_opened')
-
-  // Inject data ke input name
+  popupEdit.classList.toggle('popup_opened');
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
 
 // Section 3: Mendaftarkan Event Listener
-// ---------------------------------------
-
 editButton.addEventListener('click', editProfile);
 
 saveEditProfile.addEventListener('submit', function (event) {
@@ -159,55 +115,43 @@ saveEditProfile.addEventListener('submit', function (event) {
   profileAbout.textContent = inputAbout.value;
 
   popupEdit.classList.toggle('popup_opened');
-
 });
 
 addButton.addEventListener('click', function() {
-  console.log('add button')
-
-  popupAdd.classList.toggle('popup_opened')
+  popupAdd.classList.toggle('popup_opened');
 });
 
-// Menggunakan event delegation untuk menangani tombol "Tutup" pada semua popup
 document.addEventListener('click', function (event) {
   if (event.target.classList.contains('popup__close-button')) {
-      // Cari popup yang mengandung tombol "Tutup" yang ditekan
-      const popup = event.target.closest('.popup');
-      
-      if (popup) {
-          popup.classList.remove('popup_opened'); // Sembunyikan popup saat tombol "Tutup" ditekan
-      }
+    const popup = event.target.closest('.popup');
+    if (popup) {
+      popup.classList.remove('popup_opened');
+    }
   }
 });
 
 saveAdd.addEventListener('submit', function (event) {
-  event.preventDefault()
+  event.preventDefault();
   const title = inputTitle.value;
   const image = inputImage.value;
-
-  // Validation
-
-  console.log(`
-    Title: ${title},
-    Image: ${image}
-  `);
 
   boxCards.push({
     name: title,
     link: image
   });
 
-  renderTemplate(boxCards);
-  inputTitle.value = ''
-  inputImage.value = ''
+  const card = createCard({ name: title, link: image });
+  elements.prepend(card);
+
+  inputTitle.value = '';
+  inputImage.value = '';
 
   popupAdd.classList.toggle('popup_opened');
 });
 
 closeImageButton.addEventListener('click', function () {
-  popupImages.classList.toggle('popup_opened')
+  popupImages.classList.toggle('popup_opened');
 });
 
-// --------
 // Inisialisasi
 renderTemplate(boxCards);
